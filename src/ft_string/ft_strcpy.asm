ed505e88af8cd35cb5e83e10b47fc2188b0d4eb7
Primoz Merku <prmerku@f0r4s23.codam.nl>
2020-03-10 16:00:23 +0100

    Optimized strcpy

--- src/ft_string/ft_strcpy.asm
+++ src/ft_string/ft_strcpy.asm
@@ -16,20 +16,29 @@ section .text
 	global FN_LABEL(ft_strcpy)
 
 FN_LABEL(ft_strcpy):
-	xor		edx, edx				;clear rdx register
-	xor		ecx, ecx				;clear rcx register
-	cmp		rsi, 0x0				;check if src is NULL
-	jz		.return					;return dst in case src is NULL
-
-.loop:
-	mov		dl, byte [rsi + rcx]	;copy byte from src to a tmp location
-	mov		byte [rdi + rcx], dl	;copy from tmp location to dst byte
-	cmp		dl, 0x0					;check if end fo string
-	jz		.return					;return if end of string
-
-	inc		rcx						;increment index in string src by 1
-	jmp		.loop					;jump back to loop
-
-.return:
-	mov		rax, rdi				;move dst to the return register
-	ret								;return dst
+	mov			r10,		rdi									; Store our destination string for the return value
+	mov			rax,		-16									; Initialize to -16 since we increase by 16 first round
+	mov			rcx,		rsi									; Move source to RCX for SSE4.2
+	pxor		xmm0,		xmm0								; Zero out XMM0 for PCMPISTRI mask
+	jmp			strlen_loop										; Jump to the strlen loop, skipping the first copy
+																;
+strlen_cpy:														;
+	movdqu		oword [rdi + rax],	xmm1						; Copy 16 bytes from source to destination
+																;
+strlen_loop:													;
+	add			rax,				16							; Move 16 bytes forward
+	movdqu		xmm1,				oword [rsi + rax]			; Load the data from our source string
+	pcmpistri	xmm0,				xmm1,	SSE42_EQUAL_EACH	; Check if this string contains a zero-byte
+	jnz			strlen_cpy										; If not, copy to dest and jump back to looping
+																;
+	add			rsi,				rax							; Increment our offset in the source string
+	add			rdi,				rax							; Increment our offset in the destination string
+																;
+remainder_loop:													;
+	lodsb														; Load char from source into al, increase offset
+	stosb														; Store char into destination from al, increase offset
+	test		al,		al										; Check if the character was a string terminator
+	jne			remainder_loop									; If not, keep copying characters
+																;
+	mov			rax,		r10									; Load the original destination string
+	ret															; And return the destination string
