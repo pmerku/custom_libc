6b4557d7a9ca3edb23d4e9be7b867811ff4606a3
Primoz Merku <prmerku@f0r4s23.codam.nl>
2020-03-11 10:01:39 +0100

    Fixed compilation errors

--- tests/main.c
+++ tests/main.c
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include <ft_libft.h>
 #include <fcntl.h>
-#include <stdio.h>
+#include <ft_memory.h>
 
 int 	main(void)
 {
@@ -23,13 +23,20 @@ int 	main(void)
 	char	*line;
 	int		s0;
 
-	fd0 = open("tests/big.txt", O_RDONLY);
+	fd0 = open("tests/main.c", O_RDONLY);
 	s0 = 1;
 	while (s0 > 0)
 	{
 		s0 = get_next_line(fd0, &line);
-		printf("%s\n", line);
+		ft_printf("%s\n", line);
 		free(line);
 	}
+	char	*s = ft_malloc(8);
+	ft_bzero(s, 8);
+	ft_printf("%d\n", ft_strlen(s));
+	ft_printf("%s\n", ft_strdup("helpme"));
+	ft_printf("%s\n", ft_strcpy(s, "helpme"));
+	ft_printf("%d\n", ft_strcmp(s, "help"));
+	ft_printf("%s\n", ft_memcpy(s, "hello", 6));
 	return (0);
 }
