4008e3b5210f282db1ecd82d9a5f0750bf37e8a3
pmerku <pmerku@gmail.com>
2020-10-10 14:48:55 +0200

    Update

--- tests/main.cpp
+++ tests/main.cpp
@@ -16,27 +16,52 @@
 #include <ft_libft.h>
 #include <fcntl.h>
 #include <ft_memory.h>
+#include <MutantStack.hpp>
+#include <iostream>
+#include <vector>
+#include <unistd.h>
 
 int 	main(void)
 {
-	int		fd0;
+	int		fd;
 	char	*line;
-	int		s0;
+	int		s0 = 1;
 
-	fd0 = open("tests/main.c", O_RDONLY);
-	s0 = 1;
-	while (s0 > 0)
-	{
-		s0 = get_next_line(fd0, &line);
-		ft_printf("[%d][%d]=> [%s]\n", fd0, s0, line);
+	if ((fd = open("./tests/main.cpp", O_RDONLY)) == -1) {
+		std::cout << "Error" << std::endl;
+		return 1;
+	}
+	while (s0 > 0) {
+		s0 = get_next_line(fd, &line);
+		ft_printf("[%d][%d]=> [%s]\n", fd, s0, line);
 		free(line);
 	}
-	char	*s = ft_malloc(8);
+	close(fd);
+	char	*s = static_cast<char *>(ft_malloc(8));
 	ft_bzero(s, 8);
 	ft_printf("%d\n", ft_strlen(s));
 	ft_printf("%s\n", ft_strdup("helpme"));
 	ft_printf("%s\n", ft_strcpy(s, "helpme"));
 	ft_printf("%d\n", ft_strcmp(s, "help"));
 	ft_printf("%s\n", ft_memcpy(s, "hello", 6));
-	return (0);
+
+	std::cout << "-----------------\n";
+	MutantStack<int, std::vector<int> > vstack;
+
+	for (int i = 0; i < 11; ++i) {
+		vstack.push(rand() % 100);
+	}
+	std::cout << "top: " << vstack.top() << "\n";
+	vstack.pop();
+	std::cout << "size: " << vstack.size() << "\n";
+	MutantStack<int, std::vector<int> >::const_iterator it = vstack.begin();
+	MutantStack<int, std::vector<int> >::const_iterator ite = vstack.end();
+	std::cout << "VStack = ";
+	while (it != ite) {
+		std::cout << *it << " | ";
+		++it;
+	}
+	vstack.pop();
+	std::cout << "\ntop: " << vstack.top() << '\n';
+	return 0;
 }
