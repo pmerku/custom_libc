85d5bc00387132cf4f028f9aa5f6e5096992d37e
pmerku <pmerku@gmail.com>
2020-10-13 20:43:48 +0200

    Initial ft_llist_last

--- src/ft_llist/ft_llist_last.c
+++ src/ft_llist/ft_llist_last.c
@@ -10,45 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#ifndef FT_LLIST_H
-# define FT_LLIST_H
+#include <stddef.h>
+#include <ft_llist.h>
 
-/*
-** Private struct - don't use.
-*/
-typedef struct			s_llist_node {
-	void				*data;
-	struct s_llist_node	*next;
-}						t_llist_node;
-
-typedef struct			s_llist {
-	void				(*del_fn)(void *);
-	t_llist_node		*head;
-}						t_llist;
-
-t_llist					*ft_llist_new(void (*del_fn)(void *));
-void					ft_llist_push_front(t_llist *lst, void *data);
-void					ft_llist_push_back(t_llist *lst, void *data);
-int						ft_llist_size(t_llist *lst);
-t_llist_node			*ft_llist_last(t_llist *lst);
-
-/*
-** FREES the linked list, setting the list to null
-*/
-void					ft_llist_free(t_llist **lst);
-
-/*
-** CLEARS the linked list, not setting the list to null, but removing all
-** entries
-*/
-void					ft_llist_clear(t_llist *lst);
-
-void					ft_llist_iter(t_llist *lst, void (*f)(void *));
-
-#endif
-#ifdef __cplusplus
+t_llist_node	*ft_llist_last(t_llist *lst) {
+	if (lst->head == NULL) {
+		return NULL;
+	}
+	t_llist_node	*node = lst->head;
+	while (node != NULL) {
+		node = node->next;
+	}
+	return (node);
 }
-#endif
